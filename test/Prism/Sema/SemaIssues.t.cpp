8aa5618735474493e67413b2740f468161a7f27f
chrysante <gruenlilie@protonmail.com>
2024-12-30 22:31:15 +0100

    test(sema): Add tests for sema diagnostics

--- test/Prism/Sema/SemaIssues.t.cpp
+++ test/Prism/Sema/SemaIssues.t.cpp
@@ -9,5 +9,16 @@ TEST_CASE("Undeclared ID", "[sema]") {
     auto c = IssueChecker::Make(R"(
 fn foo(arg: Bar) {}
 )");
-    CHECK(c.find<UndeclaredID>());
+    CHECK(c.findOnLine<UndeclaredID>(2));
+}
+
+TEST_CASE("Typedef cycle", "[sema]") {
+    auto c = IssueChecker::Make(R"(
+struct Foo { var bar: Bar; }
+struct Bar { var foo: Foo; }
+)");
+    auto* diag = c.find<TypeDefCycle>();
+    REQUIRE(diag);
+    CHECK(c.findOnLine<SemaNote>(*diag, 2));
+    CHECK(c.findOnLine<SemaNote>(*diag, 3));
 }
--- test/Prism/TestUtils/TestCompiler.h
+++ test/Prism/TestUtils/TestCompiler.h
@@ -1,7 +1,13 @@
+#ifndef PRISM_TESTUTILS_TESTCOMPILER_H
+#define PRISM_TESTUTILS_TESTCOMPILER_H
+
 #include <string>
 
 #include <range/v3/algorithm.hpp>
+#include <range/v3/view.hpp>
 
+#include "Prism/Common/Assert.h"
+#include "Prism/Common/Functional.h"
 #include "Prism/Common/Issue.h"
 #include "Prism/Common/IssueHandler.h"
 #include "Prism/Invocation/Invocation.h"
@@ -15,13 +21,26 @@ public:
 
     template <std::derived_from<Issue> I>
     I const* findOnLine(int line) {
-        assert(false);
-        return nullptr;
+        return findImpl<I>(invocation.getIssueHandler(), onLineFn<I>(line));
+    }
+
+    template <std::derived_from<Issue> I>
+    I const* findOnLine(Issue const& issue, int line) {
+        return findImpl<I>(issue.children() |
+                               ranges::views::transform(Dereference),
+                           onLineFn<I>(line));
     }
 
     template <std::derived_from<Issue> I>
     I const* find() {
-        return findImpl<I>(Isa<I>);
+        return findImpl<I>(invocation.getIssueHandler(), Isa<I>);
+    }
+
+    template <std::derived_from<Issue> I>
+    I const* find(Issue const& issue) {
+        return findImpl<I>(issue.children() |
+                               ranges::views::transform(Dereference),
+                           Isa<I>);
     }
 
 private:
@@ -29,14 +48,26 @@ private:
     static constexpr auto Isa =
         [](auto& p) { return dynamic_cast<T const*>(&p) != nullptr; };
 
+    template <typename I>
+    static auto onLineFn(int line) {
+        PRISM_ASSERT(line > 0);
+        return [=](auto& issue) {
+            if (!Isa<I>(issue)) return false;
+            auto range = issue.sourceRange();
+            return range && range->begin.line + 1 == (uint32_t)line;
+        };
+    }
+
     template <std::derived_from<Issue> I>
-    I const* findImpl(auto condition) {
-        auto& iss = invocation.getIssueHandler();
-        auto itr = ranges::find_if(iss, condition);
-        return itr != iss.end() ? dynamic_cast<I const*>(&*itr) : nullptr;
+    I const* findImpl(auto&& rng, auto condition) {
+        auto itr = ranges::find_if(rng, condition);
+        return itr != ranges::end(rng) ? dynamic_cast<I const*>(&*itr) :
+                                         nullptr;
     }
 
     Invocation invocation;
 };
 
 } // namespace prism
+
+#endif // PRISM_TESTUTILS_TESTCOMPILER_H
