f67abd598c8a08eec98ef9fc0050a4d278dc420d
chrysante <gruenlilie@protonmail.com>
2025-04-05 12:04:29 +0200

    chore: improve build system

--- tool/configure.sh
+++ tool/configure.sh
@@ -22,4 +22,4 @@ else
     error "Unknown OS \"$OS\""
 fi
 
-cmake -S $PROJ_DIR -B $PROJ_DIR/build $GENERATOR -DCPM_SOURCE_CACHE=build/srccache $@
+cmake -S $PROJ_DIR -B $PROJ_DIR/build $GENERATOR -DCPM_SOURCE_CACHE=.cache $@
