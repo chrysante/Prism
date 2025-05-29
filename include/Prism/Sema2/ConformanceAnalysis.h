125bf8d9f20b23b51a87a93c7dca24f34412038e
chrysante <gruenlilie@protonmail.com>
2025-05-29 09:23:38 +0200

    chore: add missing include guard

--- include/Prism/Sema2/ConformanceAnalysis.h
+++ include/Prism/Sema2/ConformanceAnalysis.h
@@ -1,3 +1,6 @@
+#ifndef PRISM_SEMA2_CONFORMANCEANALYSIS_H
+#define PRISM_SEMA2_CONFORMANCEANALYSIS_H
+
 #include <Prism/Sema2/SemaFwd.h>
 
 namespace prism {
@@ -8,3 +11,5 @@ void analyze_trait_conformances(SemaContext& ctx, DiagnosticEmitter& DE,
                                 Module& mod);
 
 } // namespace prism
+
+#endif // PRISM_SEMA2_CONFORMANCEANALYSIS_H
